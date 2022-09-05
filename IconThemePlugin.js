be651e518651f622ac36fa3ee363282df5896763
Aaron Meese <ajmeese7@gmail.com>
2022-09-05 23:21:43 +0000

    Made requested changes

--- IconThemePlugin.js
+++ IconThemePlugin.js
@@ -1,34 +1,23 @@
 class IconThemePlugin {
-  // Define `apply` as its prototype method which is supplied with compiler as its argument
   apply(compiler) {
     const pluginName = IconThemePlugin.name;
     const { webpack } = compiler;
     const { RawSource } = webpack.sources;
 
-    // Specify the event hook to attach to
     compiler.hooks.emit.tap(pluginName, (compilation) => {
       const icons = compilation.getAssets().map(
         (asset) => asset.name.split('/').pop()
       );
 
-      // Create an object with the property name as the asset name
-      // and the value as the file extension
-      const iconTypes = icons.reduce(
-        (acc, icon) => {
-          const [name, ext] = icon.split('.');
-          acc[name] = ext;
-          return acc;
-        },
-        {}
-      );
+      const iconEntries = icons.map((file) => [
+        file.substr(0, file.lastIndexOf(".")),
+        file.substr(file.lastIndexOf(".") + 1, file.length)
+      ]);
+      const iconTypes = Object.fromEntries(iconEntries);
 
-      // Read the current metadata file
       const metadata = require('./metadata.json');
-      // Overwrite the icons property with the new icon types
       metadata.icons = iconTypes;
-      // Convert the metadata object to a JSON string
       const json = JSON.stringify(metadata, null, 2);
-      // Create a new asset with the metadata.json file name
       compilation.emitAsset('../metadata.json', new RawSource(json));
     });
   }
