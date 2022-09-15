364e823095eae7464c6702464f9dec4d5714607e
Aaron Meese <ajmeese7@gmail.com>
2022-09-15 15:21:14 -0400

    Add Webpack IconThemePlugin (#27)
    Adds a Webpack plugin that populates package metadata with available resources.
    
    This can be used for icon resolution in the client so that extensions can be implicit,
    and would be able to use non-png icons.

---
+++ IconThemePlugin.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+class IconThemePlugin {
+  constructor(options = {}) {
+    this.metadataPath = options.metadataPath;
+  }
+
+  apply(compiler) {
+    const pluginName = IconThemePlugin.name;
+    const {webpack: {RawSource}} = compiler;
+
+    compiler.hooks.emit.tap(pluginName, (compilation) => {
+      const icons = compilation.getAssets().map(
+        (asset) => asset.name.split('/').pop()
+      );
+
+      const metadataFile = `${this.metadataPath || compiler.context}/metadata.json`;
+      const metadata = require(metadataFile);
+
+      const iconEntries = icons.map((file) => [
+        file.substr(0, file.lastIndexOf('.')),
+        file.substr(file.lastIndexOf('.') + 1, file.length)
+      ]);
+      iconEntries.sort(([a], [b]) => a.localeCompare(b));
+
+      const imageTypes = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp'];
+      metadata.icons = Object.fromEntries(
+        Object.entries(iconEntries).filter(
+          ([key, value]) => imageTypes.includes(value)
+        )
+      );
+
+      const json = JSON.stringify(metadata, null, 2);
+      const relativePath = path.relative(compiler.outputPath, metadataFile);
+      compilation.emitAsset(relativePath, new RawSource(json));
+    });
+  }
+}
+
+module.exports = IconThemePlugin;
