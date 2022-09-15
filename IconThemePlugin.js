f032c3bb7d55c0d93802a68b0c87b8eb5647943d
Aaron Meese <ajmeese7@gmail.com>
2022-09-15 18:36:00 +0000

    Made requested changes

--- IconThemePlugin.js
+++ IconThemePlugin.js
@@ -1,24 +1,38 @@
 const path = require('path');
 
 class IconThemePlugin {
+  constructor(options = {}) {
+    this.metadataPath = options.metadataPath;
+  }
+
   apply(compiler) {
     const pluginName = IconThemePlugin.name;
-    const { webpack } = compiler;
-    const { RawSource } = webpack.sources;
+    const {webpack} = compiler;
+    const {RawSource} = webpack.sources;
 
     compiler.hooks.emit.tap(pluginName, (compilation) => {
       const icons = compilation.getAssets().map(
         (asset) => asset.name.split('/').pop()
       );
 
+      const metadataFile = `${this.metadataPath || compiler.context}/metadata.json`;
+      const metadata = require(metadataFile);
+
       const iconEntries = icons.map((file) => [
         file.substr(0, file.lastIndexOf('.')),
         file.substr(file.lastIndexOf('.') + 1, file.length)
       ]);
 
-      const metadataFile = `${compiler.context}/metadata.json`;
-      const metadata = require(metadataFile);
-      metadata.icons = Object.fromEntries(iconEntries);
+      const sortedIcons = Object.fromEntries(
+        iconEntries.sort((a, b) => a[0].localeCompare(b[0]))
+      );
+
+      const imageTypes = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp'];
+      metadata.icons = Object.fromEntries(
+        Object.entries(sortedIcons).filter(
+          ([key, value]) => imageTypes.includes(value)
+        )
+      );
 
       const json = JSON.stringify(metadata, null, 2);
       const relativePath = path.relative(compiler.outputPath, metadataFile);
